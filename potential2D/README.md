4b57b59e3046ea171c6fb3cb84d97da79ce75e42
Dr. Xiaozhen Zhang <41996718+mkb9559@users.noreply.github.com>
2021-12-13 16:44:20 +0800

    Delete potential2D directory

--- potential2D/README.md
+++
@@ -1,6 +0,0 @@
-# potential2D
-Establish 2D potentical field by Gaussian function.
-
-# Quick Start
-- Configure init parameters.
-- Set obstacle information.
--- potential2D/SumPotential.m
+++
@@ -1,8 +0,0 @@
-function mp=SumPotential(A_max,mp1,mp2)
-mp=0*mp1;
-for i=1:length(mp(:,1))
-    for j=1:length(mp(1,:))
-        mp(i,j)=min(mp1(i,j)+mp2(i,j),A_max);
-    end
-end
-end
